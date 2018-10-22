4848bb3aa574f17b5194ce2183e86256164831b6
Alex Fedotov <alxfed@gmail.com>
2018-10-22 11:22:29 -0500

    interpreter

---
+++ 12_interpreter/interpreter.py
@@ -0,0 +1,16 @@
+"""
+interpreter.py
+"""
+
+
+class NonTerminal(object):
+    def __init__(self, expression):
+        self.expression = expression
+
+    def interpret(self):
+        self.expression.interpret()
+
+
+class Terminal(object):
+    def interpret(self):
+        pass
