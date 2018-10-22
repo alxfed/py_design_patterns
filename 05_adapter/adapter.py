4405de004b7e7da19807cb2bc4978475545206e9
Alex Fedotov <alxfed@gmail.com>
2018-10-22 12:09:01 -0500

    adapter

---
+++ 05_adapter/adapter.py
@@ -0,0 +1,18 @@
+"""
+adapter.py
+"""
+
+
+from third_party import WhatIHave
+
+
+class ObjectAdapter(object):
+    def __init__(self, what_i_have):
+        self.what_i_have = what_i_have
+
+    def required_function(self):
+        return self.what_i_have.provided_function_1()
+
+    def __getattr__(self, attr):
+        # Everything else is handled by the wrapped object
+        return getattr(self.what_i_have, attr)
